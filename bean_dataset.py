4c7007b9f947355edeb6872a82b886c2cbb43426
hiwjc <hiwjc@naver.com>
2025-06-24 22:15:31 +0900

    bean_dataset.py

--- bean_dataset.py
+++ bean_dataset.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 
 ds = load_dataset('beans')
 
-#ViTFeatureExtractor (전처리 함수.)
+#ViTFeatureExtractor (전처리 함수)
 
 from transformers import ViTFeatureExtractor
 
@@ -24,4 +24,24 @@ def transform(example_batch):
 prepared_ds = ds.with_transform(transform)
 
 
+# 버전 관련 문제 해결
+
+from datasets import load_dataset
+from transformers import ViTImageProcessor
+
+# 데이터셋 로드
+ds = load_dataset('beans')
+
+# ViTImageProcessor 이용
+model_name_or_path = 'google/vit-base-patch16-224-in21k'
+image_processor = ViTImageProcessor.from_pretrained(model_name_or_path)
+
+# 전처리 함수 정의
+def transform(example_batch):
+    inputs = image_processor([x for x in example_batch['image']], return_tensors='pt')
+    inputs['labels'] = example_batch['labels']
+    return inputs
+
+# 전처리 적용
+prepared_ds = ds.with_transform(transform)
 
